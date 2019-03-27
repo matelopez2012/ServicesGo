33ff5d33c6921c14a807b4a5e39bbc3a29e4d8ea
Cristian Castro <criscape99@gmail.com>
2019-03-27 10:10:52 -0500

    Añadiendo los metodos de Peticion

--- ServicesGo/Models/Peticion.cs
+++ ServicesGo/Models/Peticion.cs
@@ -7,5 +7,32 @@ namespace ServicesGo.Models
 {
     public class Peticion
     {
+        private string nombreCuenta;
+        private string auditor;
+        private string observacion;
+        private DateTime fechaMod;
+        private bool resuelta;
+
+        public Peticion(string nombreCuenta, string auditor, string observacion)
+        {
+            this.nombreCuenta = nombreCuenta;
+            this.auditor = auditor;
+            this.observacion = observacion;
+            this.fechaMod = DateTime.Today;
+        }
+
+        /*Gets*/
+
+        public DateTime getFechaMod()
+        {
+            return this.fechaMod;
+        }
+
+        /*Sets*/
+
+        public void setResuelta(bool resuelta)
+        {
+            this.resuelta = resuelta;
+        }
     }
 }
