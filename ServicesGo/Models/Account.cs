507e6c8026be70cd1a64d43bda1e8270b480fc80
Cristian Castro <criscape99@gmail.com>
2019-03-27 09:14:33 -0500

    Adding methos to Account

--- ServicesGo/Models/Account.cs
+++ ServicesGo/Models/Account.cs
@@ -7,5 +7,46 @@ namespace ServicesGo.Models
 {
     public class Account
     {
+        private string nombreUsuario;
+        private string contrasena;
+        private string rol;
+        private bool aprobada;
+
+        public Account(String nombreUsuario, String contrasena, String rol)
+        {
+            this.nombreUsuario = nombreUsuario;
+            this.contrasena = contrasena;
+            this.rol = rol;
+            this.aprobada = false;
+        }
+
+        /*Gets*/
+
+        public string getNombreUsuario()
+        {
+            return this.nombreUsuario;
+        }
+
+        public string getContrasena()
+        {
+            return this.contrasena;
+        }
+
+        public string getRol()
+        {
+            return this.rol;
+        }
+
+        public bool getAprobada()
+        {
+            return this.aprobada;
+        }
+
+        /*Sets*/
+
+        public void setAprobada(bool aprobada)
+        {
+            this.aprobada = aprobada;
+        }
     }
 }
--- ServicesGo/Models/Profession.cs
+++ ServicesGo/Models/Profession.cs
@@ -7,5 +7,8 @@ namespace ServicesGo.Models
 {
     public class Profession
     {
+
+        
+
     }
 }
