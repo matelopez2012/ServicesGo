f8d15d2ec4ed1fbea91a0c865904eda18f36c100
cristian camilo rios giraldo <CCRIOS@hotmail.es>
2019-03-29 11:22:44 -0500

    update class person

--- ServicesGo/Models/Empleado.cs
+++ ServicesGo/Models/Empleado.cs
@@ -10,26 +10,14 @@ namespace ServicesGo.Models
 public class Empleado : Persona
 
 {
+
         public Empleado(string nombreUsuario, string nombre, string apellidos, string cedula, string direccion, string telefono, string correoElectronico, string foto)
+            : base(nombreUsuario,  nombre,  apellidos,  cedula, direccion,  telefono,  correoElectronico,  foto)
         {
-            this.nombreUsuario = nombreUsuario;
-            this.nombre = nombre;
-            this.apellidos = apellidos;
-            this.cedula = cedula;
-            this.direccion = direccion;
-            this.telefono = telefono;
-            this.correoElectronico = correoElectronico;
-            this.foto = foto;
+            
         }
 
-        private override string nombreUsuario { get; set; }
-        private override string nombre { get; set; }
-        private override string apellidos { get; set; }
-        private override string cedula { get; set; }
-        private override string direccion { get; set; }
-        private override string telefono { get; set; }
-        private override string correoElectronico { get; set; }
-        private override string foto { get; set; }
+        
     }
 
 }
--- ServicesGo/Models/PrestadorServicios.cs
+++ ServicesGo/Models/PrestadorServicios.cs
@@ -7,59 +7,51 @@ using System.Web;
 
 namespace ServicesGo.Models
 {
-    public class PrestadorServicios : Person
+    public class PrestadorServicios : Persona
 
     {
+
+        private int estiloPresentacion { get; set; }
+        private int formatoHV { get; set; }
+        private bool modificado { get; set; }
+        private DateTime fechaModificacion { get; set; }
+        private List<Habilidad> habilidades { get; set; }
+        private Documento arl { get; set; }
+        private Documento socialSecurity { get; set; }
+
         public PrestadorServicios(string nombreUsuario, string nombre, string apellidos, string cedula, 
             string direccion, string telefono, string correoElectronico, string foto, List<Habilidad> habilidades,
             string nombreDocArl,string rutaArl ,string nombreDocSegSocial, string rutaSegSocial , int estiloPresentacion,
             int formatoHV, bool modificado, DateTime fechaModificacion)
+            : base(nombreUsuario, nombre, apellidos,  cedula, direccion,  telefono,  correoElectronico,  foto)
+
         {
-            this.nombreUsuario = nombreUsuario;
-            this.nombre = nombre;
-            this.apellidos = apellidos;
-            this.cedula = cedula;
-            this.direccion = direccion;
-            this.telefono = telefono;
-            this.correoElectronico = correoElectronico;
-            this.foto = foto;
-            this.Habilities = habilidades;
+            
+            this.habilidades = habilidades;
             this.estiloPresentacion = estiloPresentacion;
             this.formatoHV = formatoHV;
             this.modificado = modificado;
             this.fechaModificacion = fechaModificacion;
 
             createArl(nombreDocArl, rutaArl);
-            socialSecurity(nombreDocSegSocial, rutaSegSocial);
+            createsocialSecurity(nombreDocSegSocial, rutaSegSocial);
 
         }
 
 
-        public createArl(string nombreDocArl, string rutaArl) {
-            this.arl = new Document(nombreDocArl, rutaArl);
+        public void createArl(string nombreDocArl, string rutaArl) {
+            this.arl = new Documento(nombreDocArl, rutaArl);
         }
 
-        public socialSecurity(string nombreDocSegSocial, string rutaSegSocial) {
-            this.socialSecurity = new Document(nombreDocSegSocial, rutaSegSocial);
+        public void createsocialSecurity(string nombreDocSegSocial, string rutaSegSocial) {
+            this.socialSecurity = new Documento(nombreDocSegSocial, rutaSegSocial);
 
         }
 
 
-        private int estiloPresentacion { get; set; }
-        private int formatoHV { get; set; }
-        private bool modificado { get; set; }
-        private DateTime fechaModificacion { get; set; }
-        private override string nombreUsuario { get; set; }
-        private override string nombre { get; set; }
-        private override string apellidos { get; set; }
-        private override string cedula { get; set; }
-        private override string direccion { get; set; }
-        private override string telefono { get; set; }
-        private override string correoElectronico { get; set; }
-        private override string foto { get; set; }
-        private List<Habilities> Habilities { get; set; }
-        private Document arl { get; set; }
-        private Document socialSecurity{ get; set; }
+        
+       
+        
 
 
     }
