bcfe892e480c962d0d5e035583694a2260c74636
Mateo López <matelopez2012@gmail.com>
2019-04-08 02:03:41 -0500

    Model Administrator and Model Person finished.

--- ServicesGo/Business_Layer/Models/Administrador.cs
+++ ServicesGo/Business_Layer/Models/Administrador.cs
@@ -10,10 +10,15 @@ namespace ServicesGo.Models
     public class Administrador : Persona
     {
 
-    
-        
+
+        [Key]
+        public int Id { get; set; }
         //[ForeignKey("Person_Id")]
-        public virtual Persona Persona_Id { get; set; }
+        public Persona persona  {get; set;}
+        public int PersonaId { get; set; }
+
+        [Timestamp]
+        public Byte[] TimeStamp { get; set; }
 
         public Administrador(string nombreUsuario, string nombre, string apellidos, string cedula, string direccion, string telefono, string correoElectronico, string foto)
         : base(nombreUsuario, nombre, apellidos, cedula, direccion, telefono, correoElectronico, foto)
--- ServicesGo/Business_Layer/Models/Persona.cs
+++ ServicesGo/Business_Layer/Models/Persona.cs
@@ -37,6 +37,9 @@ namespace ServicesGo.Models
         [Required(ErrorMessage = "El campo {0} es requerido")]
         public string foto { get; set; }
 
+        [Timestamp]
+        public Byte[] TimeStamp { get; set; }
+
         public Persona(string nombreUsuario, string nombre, string apellidos, string cedula, string direccion, string telefono, string correoElectronico, string foto)
         {
             this.nombreUsuario = nombreUsuario;
@@ -50,56 +53,6 @@ namespace ServicesGo.Models
         }
 
     }
-    /*
-        public string nombreUsuario
-        {
-            get => _nombreUsuario;
-            set => _nombreUsuario = value;
-        }
 
-        public string nombre
-        {
-            get { return _nombre; }
-            set { _nombre = value; }
-        }
-
-        public string apellidos
-        {
-            get { return _apellidos; }
-            set { _apellidos = value; }
-        }
-
-        public string cedula
-        {
-            get { return _cedula; }
-            set { _cedula = value; }
-        }
-
-        public string direccion
-        {
-            get { return _direccion; }
-            set { _direccion = value; }
-        }
-
-        public string telefono
-        {
-            get { return _telefono; }
-            set { _telefono = value; }
-        }
-
-
-        public string correoElectronico
-        {
-            get { return _correoElectronico; }
-            set { _correoElectronico = value; }
-        }
-
-        public string foto
-        {
-            get { return _foto; }
-            set { _foto = value; }
-        }
-
-    */
-    }
+}
 
