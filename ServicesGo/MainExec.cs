5dda2397ac190f10bbb1de05905738657dcafd9a
Cristian Castro <criscape99@gmail.com>
2019-04-12 02:05:28 -0500

    Probando errores

---
+++ ServicesGo/MainExec.cs
@@ -0,0 +1,21 @@
+﻿using ServicesGo.Controllers.BussinesLayer.ControladorasHabilidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServicesGo.Business_Layer.Controllers.ControladorasCuenta;
+
+namespace ServicesGo
+{
+    public class MainExec
+    {
+
+        static void Main()
+        {
+            ControladoraCrearCuenta.crearCuenta("juanse_duque", "random", "usuario");
+            Console.WriteLine(ControladoraValidarInicioSesion.validarSesion("juanse_duque","random"));
+        }
+       
+
+    }
+}
