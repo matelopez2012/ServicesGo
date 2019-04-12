f44196b3a178d87eadf7dd40e27245b5c596b1a9
Cristian Castro <criscape99@gmail.com>
2019-04-12 00:20:18 -0500

    Añadiendo las controladoras de peticion

---
+++ ServicesGo/Business_Layer/Controllers/ControladorasPeticion/ControladorDenegarPeticion.cs
@@ -0,0 +1,29 @@
+﻿using ServicesGo.Models;
+using ServicesGo.Persistence_Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesGo.Business_Layer.Controllers.ControladorasPeticion
+{
+    public class ControladorDenegarPeticion
+    {
+        private static HomeServicesContext db = new HomeServicesContext();
+
+        public static PrestadorServicios buscarPS(string nombreUsuario)
+        {
+            var query = from st in db.PrestadoresServicios
+                        where st.nombreUsuario == nombreUsuario
+                        select st;
+
+            return query.FirstOrDefault();
+        }
+
+        public static void denegarPeticion(string idPeticion, string nombreCuenta, bool resuelta)
+        {
+            PrestadorServicios ps = buscarPS(nombreCuenta);
+            
+        }
+    }
+}
