6355bf18294557570811ac6fa4132e06bca10266
Cristian Castro <criscape99@gmail.com>
2019-04-26 07:46:19 -0500

    Working on...

--- UnitTestProject1/CrearCuentaTest.cs
+++ UnitTestProject1/CrearCuentaTest.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServicesGo.Business_Layer.Controllers.ControladorasCuenta;
+using ServicesGo.Persistence_Layer;
 
 namespace CrearCuentaTest
 {
     [TestClass]
     public class CrearCuentaTest
     {
+        private static HomeServicesContext db = new HomeServicesContext();
+
         [TestMethod]
         public void buscarCuentaExistente()
         {
+            bool esperado = true;
 
+            //db.Cuentas.Add();
+
+            ControladoraCrearCuenta.buscarCuenta("");
         }
     }
 }
