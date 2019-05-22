584c96fe614c6a3f566e1f75fc3eba96ecbb304a
sebastian <sebastianduque1323@hotmail.com>
2019-05-21 22:10:02 -0500

    Some changes

---
+++ ServicesGo/Business_Layer/Controllers/ControladorasHabilidad/ControladorMostrarHabilidades.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicesGo.Business_Layer.Controllers.ControladorasHabilidad
+{
+    public class ControladorMostrarHabilidades
+    {
+        //Creamos la instancia de HomeServicesContext que permitirá mapear la base de datos
+        private static HomeServicesContext db = new HomeServicesContext();
+
+        //Método que retorna una lista con todas las habilidades en la tabla habilidades
+        public List<Habilidad> mostrarHabilidades()
+        {
+            //Se  buscan todas las habilidades
+            List<Habilidad> habilidades = db.Habilidades.toList();
+
+            //Si la lista de habilidades es diferente de nulo, retornamos la lista
+            if (habilidades != null)
+            {
+                return habilidades;
+            }
+            //En caso contrario retornamos nulo
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
